f17d629033d7ed89b05c97a918107a064a4f4b40
unknown <ararky7@gmail.com>
2020-10-13 22:43:26 +0530

    First Name Validation Program

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,3 +1,15 @@
 #!/bin/bash -x
 
-echo "Welcome to User Registration System"
+#program to validate First Name of user
+
+echo "First Name should Start with Capital and has minimum 3 characters ";
+read -p "Enter First Name Of User" firstname;
+
+pat="^[A-Z]{3,}";
+
+if [[ $firstname =~ $pat ]]
+then
+	echo $firstname " is valid ";
+else
+	echo $firstname " is invalid ";
+fi
