ac4f03620c76ec67f42f868274c40334447061a7
unknown <ararky7@gmail.com>
2020-10-14 13:10:21 +0530

    Password contain min 8 char & 1 upper case & 1 numeric

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,13 +1,20 @@
 #!/bin/bash -x
 
-echo "Password Should contain Minimum 8 characters and at least one upper case "
+echo "Password Should contain Minimum 8 characters and at least one upper case and at least one numeric "
 read -p "Enter Password " password;
 
-pat="[A-Z0-9]{8,}"
+pat="^[0-9a-zA-Z]{8,}$"
 
 if [[ $password =~ $pat ]]
 then
-	echo $password" is valid";
+	echo $password" is having 8 characters";
+
+	if [[ "$password" =~ [A-Z] && "$password" =~ [0-9] ]];
+	then
+		echo $password " is valid";
+	else
+		echo $password " is invalid ";
+	fi
 else
 	echo $password" is invalid"
 fi
